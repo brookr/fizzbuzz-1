a9711e436ca3ec3e2cb129169f1b6f470af46cb8
amykangweb <selene6023@gmail.com>
2015-08-31 19:04:34 -0700

    Add fizzbuzz answer

---
+++ fizzbuzz.rb
@@ -0,0 +1,16 @@
+-# FizzBuzz Challenge
+-
+-def fizzbuzz(number)
+-  if number % 3 == 0 && number % 5 == 0
+-    return "FizzBuzz"
+-  elsif number % 3 == 0
+-    return "Fizz"
+-  elsif number % 5 == 0
+-    return "Buzz"
+-  else
+-    return number
+-  end
+-end
+-
+-# returns "FizzBuzz"
+-fizzbuzz(15)
